fb84eb539c484478bd397f2ddfcafac8e2581588
Thiago Santos <thiagoss@osg.samsung.com>
2016-01-22 18:17:08 -0300

    cookiescheck: add expires optional field

--- cookiescheck-test-server.py
+++ cookiescheck-test-server.py
@@ -14,13 +14,14 @@ app = Flask(__name__)
 
 filepath = None
 mainpath = None
+maxage = None
 
 @app.route('/<path:path>')
 def get(path):
     ret = make_response(send_from_directory(filepath, path))
 
     if path == mainpath:
-        ret.set_cookie('auth', '1')
+        ret.set_cookie('auth', '1', max_age=maxage)
     elif request.cookies.get('auth') == '1':
         pass
     else:
@@ -29,12 +30,14 @@ def get(path):
     return ret
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print "Usage: %s <dir-to-serve> <main-file>"
+    if len(sys.argv) not in [3, 4]:
+        print "Usage: %s <dir-to-serve> <main-file> [max-age (seconds / optional)]"
         sys.exit(1)
 
     print sys.argv
     filepath = sys.argv[1]
     mainpath = sys.argv[2]
+    if len(sys.argv) > 3:
+      maxage = int(sys.argv[3])
     app.run(host='0.0.0.0')
 
